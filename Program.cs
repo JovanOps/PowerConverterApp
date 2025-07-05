b3e7bf37fc38f02c03ea264baba9fde16feae1cf
Korisnik <Korisnik@DESKTOP-SGVK0U9>
2025-07-06 00:37:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+
+            Console.WriteLine("Please, enter value: ");
+            bool isValidInput = double.TryParse(Console.ReadLine(), out double value);
+
+            if (!isValidInput)
+            {
+                Console.WriteLine("Invalid number input.");
+                return;
+            }
+
+            Console.WriteLine("Convert to (enter kw or hp):");
+            string convertTo = Console.ReadLine().ToLower();
+
+            if (convertTo == "kw")
+            {
+                result = HpToKw(value); // korisnik uneo HP -> konvertujemo u KW
+            }
+            else if (convertTo == "hp")
+            {
+                result = KwToHp(value); // korisnik uneo KW -> konvertujemo u HP
+            }
+            else
+            {
+                Console.WriteLine("Invalid conversion unit.");
+                return;
+            }
+
+            Console.WriteLine("Converted result: " + result);
+        }
+
+        private static double KwToHp(double kw)
+        {
+            return kw / 0.745699872;
+        }
+
+        private static double HpToKw(double hp)
+        {
+            return hp * 0.745699872;
+        }
+    }
+}
